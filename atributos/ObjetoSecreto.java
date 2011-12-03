c132b320998fec2199b1a24206d8bcffd13dc699
Luciano Ramalho <luciano@ramalho.org>
2011-12-02 22:39:55 -0200

    exemplo que lista todos os campos privados

--- atributos/ObjetoSecreto.java
+++ atributos/ObjetoSecreto.java
@@ -1,6 +1,7 @@
 public class ObjetoSecreto {
 
 	private String escondido = "";
+	private String oculto = "dado ultra secreto";
 
 	public ObjetoSecreto(String texto) {
 		this.escondido = texto;
--- atributos/Teste.java
+++ atributos/Teste.java
@@ -1,22 +1,27 @@
 import java.lang.reflect.Field;
-import java.lang.NoSuchFieldException;
-import java.lang.IllegalAccessException;
 
 public class Teste {
 
 	public static void main(String[] args) {
 		ObjetoSecreto oSecreto = new ObjetoSecreto("senha super secreta");
+		String tavaEscondido = null;
+		Field campoPrivado = null;
 		try {
-			Field campoPrivado = ObjetoSecreto.class.getDeclaredField("escondido");
-			campoPrivado.setAccessible(true); // arrombamos a porta
-			String tavaEscondido = (String) campoPrivado.get(oSecreto);
-			System.out.println("oSecreto.escondido = " + tavaEscondido);
+			campoPrivado = ObjetoSecreto.class.getDeclaredField("escondido");
 		}
 		catch (NoSuchFieldException e) {
 			System.err.println(e);
-		}	
-		catch (IllegalAccessException e) {
+			System.exit(1);
+		}
+		campoPrivado.setAccessible(true); // arrombamos a porta
+		try {
+			tavaEscondido = (String) campoPrivado.get(oSecreto);
+		}
+		catch (IllegalAccessException e) { 
+			// esta exceção nao acontece porque fizemos setAcessible(true)
 			System.err.println(e);
+			System.exit(1);
 		}	
+		System.out.println("oSecreto.escondido = " + tavaEscondido);
 	}
 }
