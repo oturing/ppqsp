18c53fd86f7fceab664882a33e94e9fa62c1b9f6
Luciano Ramalho <luciano@ramalho.org>
2011-12-03 05:04:39 -0200

    tabs -> espacos

--- atributos/java/ListaPrivados.py
+++ atributos/java/ListaPrivados.py
@@ -4,7 +4,7 @@ import ObjetoSecreto
 oSecreto = ObjetoSecreto('senha super secreta')
 campos = ObjetoSecreto.getDeclaredFields()
 for campo in campos:
-	if Modifier.isPrivate(campo.getModifiers()): # so campos privados!
-		print campo
-		campo.setAccessible(True) # arrombamos a porta
-		print '\t', campo.getName(), '=', campo.get(oSecreto)
+    if Modifier.isPrivate(campo.getModifiers()): # so campos privados!
+        print campo
+        campo.setAccessible(True) # arrombamos a porta
+        print '\t', campo.getName(), '=', campo.get(oSecreto)
