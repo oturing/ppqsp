c4ec9f2e9a2d9d7c04300edd354401c54317a20d
Luciano Ramalho <luciano@ramalho.org>
2011-12-13 07:19:52 -0200

    exemplo mod11 cpf

--- atributos/mod11.py
+++ atributos/mod11.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 import string
 
 def mod11(digitos):
@@ -18,11 +20,31 @@ def cpf_valido(digitos):
     dv = mod11_cpf(digitos[:-2])
     return dv == digitos[-2:]
 
-cpfs = '030.166.509-59 102.700.605-10 568.755.847-00'.split()
+def testes():
+    # CPFs de deputados listados em um processo publicado na Web
+    cpfs = '''102.700.605-10 216.307.552-49 242.544.011-91 568.755.847-00
+              006.375.133-04 348.780.507-30 244.884.451-87 254.052.509-10
+              030.880.653-00 117.703.681-91 825.025.018-49
+              030.166.509-59'''.split()
 
-for cpf in cpfs:
-    print cpf, mod11_cpf(cpf[:-2]), cpf_valido(cpf)
+    for cpf in cpfs:
+        assert cpf_valido(cpf)
+        print cpf, mod11_cpf(cpf[:-2]), cpf_valido(cpf)
 
-for i in range(10):
-    s = str(i)*9
+    for i in range(10):
+        s = str(i)*9
+        print s, mod11_cpf(s)
+
+    s = '012345678'
     print s, mod11_cpf(s)
+    s = s[::-1]
+    print s, mod11_cpf(s)
+    s = '123456789'
+    print s, mod11_cpf(s)
+    s = s[::-1]
+    print s, mod11_cpf(s)
+    s = '123454321'
+    print s, mod11_cpf(s)
+
+if __name__=='__main__':
+    testes()
