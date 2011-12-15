f97f31a8a0ccf5665288cd069f138f21de07a0ea
Luciano Ramalho <luciano@ramalho.org>
2011-12-15 09:32:04 -0200

    exemplo com tornado.gen.engine - co-rotina

---
+++ async/assincrono_ge.py
@@ -0,0 +1,73 @@
+from time import time
+from urllib2 import urlopen
+import os
+import shutil
+from itertools import takewhile
+from tornado import httpclient, ioloop, gen
+
+BASE_URL = ('https://www.cia.gov/library/publications/the-world-factbook'
+            '/graphics/flags/large/')
+
+DESTINO = './bandeiras/'
+
+try:
+    shutil.rmtree(DESTINO)
+except OSError:
+    pass
+
+os.mkdir(DESTINO)
+
+t0 = time()
+qt_bytes = 0
+qt_baixar = 0
+baixar = set()
+
+@gen.engine
+def buscar(index, nome):
+    global qt_bytes
+    baixar.add(nome)
+    response = yield gen.Task(http_client.fetch, BASE_URL+nome)
+    if response.error:
+        print "Error:", response.error
+    else:
+        # nome = response.request.url[len(BASE_URL):]
+        print index+1, nome
+        with open(DESTINO+nome, 'wb') as img_local:
+            img_local.write(response.body)
+        qt_bytes += len(response.body)
+        baixar.remove(nome)
+        if not baixar:
+            ioloop.IOLoop.instance().stop()
+
+http_client = httpclient.AsyncHTTPClient()
+
+with open('bandeiras.txt') as nomes:
+    ateh_b = takewhile(lambda s: s[0] in 'ab', nomes)
+    for index, nome in enumerate(ateh_b):
+        nome = nome.strip()
+        buscar(index, nome)
+    qt_baixar = len(baixar)
+
+ioloop.IOLoop.instance().start()
+
+print qt_bytes, 'bytes baixados em %s arquivos' % qt_baixar
+print 'tempo transcorrido:', time()-t0
+
+"""
+sincrono.py:
+    ...
+    29 bc-lgflag.gif
+    30 bv-lgflag.gif
+    31 br-lgflag.gif
+    228410 bytes baixados em 31 arquivos
+    tempo transcorrido: 43.5865662098
+
+assincrono.py:
+    ...
+    29 bc-lgflag.gif
+    30 bv-lgflag.gif
+    31 br-lgflag.gif
+    228410 bytes baixados em 31 arquivos
+    tempo transcorrido: 8.02110886574
+"""
+
