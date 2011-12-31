a4c8a0b9f115175f230a3ee9b78f9a7babace299
Luciano Ramalho <luciano@ramalho.org>
2011-12-31 00:56:17 -0200

    modulo para validacao de e-mail

--- atributos/email.py
+++ atributos/email.py
@@ -1,14 +1,14 @@
 
 """
-E-mail validation regex copied from Django 1.3.1
+E-mail validation regex from Django 1.3.1:
 
 http://code.djangoproject.com/svn/django/branches/releases/1.3.X/django/core/validators.py
 
-    >>> validar_email('luciano@ramalho.org')
+    >>> is_valid('luciano@ramalho.org')
     True
-    >>> validar_email('a@b.cd')
+    >>> is_valid('a@b.cd')
     True
-    >>> validar_email('x')
+    >>> is_valid('x')
     False
 
 """
@@ -20,5 +20,5 @@ email_re = re.compile(
     r'|^"([\001-\010\013\014\016-\037!#-\[\]-\177]|\\[\001-011\013\014\016-\177])*"' # quoted-string
     r')@(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+[A-Z]{2,6}\.?$', re.IGNORECASE)  # domain
 
-def validar_email(email):
+def is_valid(email):
     return bool(email_re.match(email))
