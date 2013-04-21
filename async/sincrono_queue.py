e1021d43087f7fc08ed34d875e82574d3b03de01
Luciano Ramalho <luciano@ramalho.org>
2013-04-21 00:43:08 -0300

    exemplos sincronos com threads, um deles usando queue

--- async/sincrono_queue.py
+++ async/sincrono_queue.py
@@ -3,12 +3,13 @@
 import contextlib
 import urllib2
 import threading
+from Queue import Queue
 
 from utilflags import ler_siglas, salvar, reportar, BASE_URL
 
 qt_bytes = 0
 qt_arqs = 0
-conj_baixar = set()
+fila = Queue()
 
 def baixar_uma(nome, numero):
     global qt_bytes, qt_arqs
@@ -18,21 +19,32 @@ def baixar_uma(nome, numero):
         qt_bytes += salvar(nome, img)
         qt_arqs += 1
         print '\t\t\t%3d\t%s --> salvo' % (numero, nome)
-        conj_baixar.discard(nome)
+
+NUM_THREADS = 100
+
+def worker():
+    while True:
+        nome, numero = fila.get()
+        baixar_uma(nome, numero)
+        fila.task_done()
 
 def baixar(qtd):
     """ busca e salva a quantidade ``qtd`` de bandeiras """
 
-    for num, sigla in enumerate(ler_siglas(qtd), 1):
+    # criar threads
+    for i in range(NUM_THREADS):
+        tarefa = threading.Thread(target=worker)
+        tarefa.daemon = True
+        tarefa.start()
+
+    # enfileirar tarefas
+    for numero, sigla in enumerate(ler_siglas(qtd), 1):
         # baixar bandeiras com inicial 'a' ou 'b'
         nome = sigla + '-lgflag.gif'
-        print '\t%3d\t%s' % (num, nome)
-        tarefa = threading.Thread(target=baixar_uma, args=(nome, num))
-        tarefa.start()
-        conj_baixar.add(nome)
+        print '\t%3d\t%s' % (numero, nome)
+        fila.put((nome, numero))
 
-    while conj_baixar:
-        pass
+    fila.join()
 
     return qt_bytes, qt_arqs
 
@@ -40,14 +52,15 @@ if __name__=='__main__':
     reportar(baixar)
 
 """
-$ python sincrono_threads.py 3
+$ python sincrono_queue.py 3
 baixando 3 arquivos...
       1 aa-lgflag.gif
       2 ac-lgflag.gif
       3 ae-lgflag.gif
-              1 aa-lgflag.gif --> salvo
               2 ac-lgflag.gif --> salvo
+              1 aa-lgflag.gif --> salvo
               3 ae-lgflag.gif --> salvo
 11889 bytes baixados em 3 arquivos
-tempo transcorrido: 1.08s
+tempo transcorrido: 0.93s
+
 """
